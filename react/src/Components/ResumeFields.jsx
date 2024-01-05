c452b9af77a271d657d3337a037f89aa4a660c61
White_Panther <99096313+White-Panther420@users.noreply.github.com>
2024-01-05 05:42:56 -0700

    .

--- react/src/Components/ResumeFields.jsx
+++ react/src/Components/ResumeFields.jsx
@@ -3,10 +3,51 @@ import React, { useState } from "react";
 import "../Styles/ResumeFields.css"
 import uniqid from 'uniqid';
 
-//This will hold all the different experiences (i.e. education, work, volunteer)
-let experienceObjectList = []
+let experienceObjectList = [
+  {
+    School: "Glendale Community College",
+    Degree: "Associates in Science",
+    startDate: "08/21/2017",
+    endDate: "05/14/2021",
+    location: "Arizona, U.S.",
+    Description: "",
+    "experience name": "Glendale Community College",
+    "experience type": "education"
+  },
+  {
+    School: "Arizona State University",
+    Degree: "Bachelors of Computer Science",
+    startDate: "08/14/2021",
+    endDate: "05/14/2024",
+    location: "Arizona, U.S.",
+    Description: "",
+    "experience name": "Arizona State University",
+    "experience type": "education"
+  },
+  {
+    "Company Name": "Arizona State University",
+    "Position Title": "CMEE Front Desk Receptionist",
+    startDate: "09/01/2021",
+    endDate: "04/14/2023",
+    location: "Arizona, U.S.",
+    description: "The tasks involved updating the content of web pages featuring lists of W. P. Carey events using HTML to provide accurate and timely information for students and community members. Additionally, a calling campaign and LinkedIn audits were conducted to gather undergraduate student employment outcomes, resulting in a notable increase of over 15% in responses from 2021 to 2022. Furthermore, I played a role in facilitating student check-ins for career advising appointments by effectively communicating with career coaches. These efforts aimed to enhance the overall experience and engagement for students within the academic and career services sphere.",
+    "experience name": "Arizona State University",
+    "experience type": "work experience"
+  },
+  {
+    "Company Name": "Glendale Community College",
+    "Position Title": "CSS Front Desk Receptionist",
+    startDate: "10/15/2018",
+    endDate: "01/15/2021",
+    location: "Arizona, U.S.",
+    description: "As a team member in the Career Services department from October 2018 to December 2020, I assisted over 20,000 students and community members with job-related activities, including the federal work-study process and resume and cover letter reviews. My role extended to promoting Career Services workshops and job fairs through marketing initiatives, event preparations, and onsite duties. I handled main phone line inquiries, greeted visitors professionally, and supported various office projects such as researching job trends and internships. Additionally, I contributed to data entry for weekly computer usage and department visits. Acting as a liaison, I reached out to student clubs to present career-related topics, and I organized bi-weekly tabling events to promote Career Services and encourage resource utilization among students.",
+    "experience name": "Glendale Community College",
+    "experience type": "work experience"
+  }
+]
 
 function ResumeFields() {
+  //This will hold all the different experiences (i.e. education, work, volunteer)
   return (
     <div className="fields-wrapper flex">
       <PersonalDetails />
